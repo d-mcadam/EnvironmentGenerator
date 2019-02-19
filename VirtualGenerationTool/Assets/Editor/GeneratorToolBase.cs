13d31c50f909fce4cdf78f9692a3a64ac086e464
dylan mcadam <dylan@dylanmcadam.co.uk>
2019-02-19 16:37:15 +0000

    Generating basic terrain object

---
+++ VirtualGenerationTool/Assets/Editor/GeneratorToolBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GeneratorToolBase : ScriptableWizard {
+
+    private const string _menuDropDownName = "Custom Generator Tool";
+
+    [MenuItem(_menuDropDownName + "/Generate Terrain")]
+    static void GenerateTerrain()
+    {
+
+        GameObject terrain = new GameObject();
+        TerrainData terrainData = new TerrainData();
+        terrain = Terrain.CreateTerrainGameObject(terrainData);
+
+    }
+}
